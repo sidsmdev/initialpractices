370f308865a9cf2bb0ec38096202049edd51d003
sidsmdev <sidsm.dev@gmail.com>
2020-04-28 16:46:20 +0530

    a learning added

--- org/lambda/practice/sid/Test.java
+++ org/lambda/practice/sid/Test.java
@@ -84,4 +84,31 @@ public class Test {
     };
 
     Function<Apples, Integer> returnLen = (a) -> {return a.getColor().length();};
+
+
+    /**
+     *  All the lambda expressions we’ve shown so far used only their arguments inside their
+        body. But lambda expressions are also allowed to use free variables (variables that aren’t
+        the parameters and are defined in an outer scope) like anonymous classes can.
+        They’re called capturing lambdas. For example, the following lambda captures the vari-
+        able portNumber:
+        int portNumber = 1337;
+        Runnable r = () -> System.out.println(portNumber);
+
+        Nonetheless, there’s a small twist. There are some restrictions on what you can do with
+        these variables. Lambdas are allowed to capture (to reference in their bodies)
+        instance variables and static variables without restrictions. But when local variables are
+        captured, they have to be explicitly declared final or be effectively final . Lambda
+        expressions can capture local variables that are assigned to only once. (Note: captur-
+        ing an instance variable can be seen as capturing the final local variable this.)
+
+        Instance variables are stored on the heap, whereas local variables live on the
+        stack. If a lambda could access the local variable directly and the lambda was used in a
+        thread, then the thread using the lambda could try to access the variable after the
+        thread that allocated the variable had deallocated it. Hence, Java implements access to
+        a free local variable as access to a copy of it, rather than access to the original variable.
+        This makes no difference if the local variable is assigned to only once—hence the
+        restriction.
+
+     */
 }
